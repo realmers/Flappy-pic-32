3e8377cae7963ef78210ec9a2295e97c3d5a897f
Alexander Danho <danhoalexander123@gmail.com>
2024-02-27 12:05:27 +0100

    refactored

--- mipslabwork.c
+++ mipslabwork.c
@@ -1,192 +1,113 @@
-/* mipslabwork.c
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
 
-   This file written 2015 by F Lundevall
-   Updated 2017-04-21 by F Lundevall
-
-   This file should be changed by YOU! So you must
-   add comment(s) here with your name(s) and date(s):
-
-   This file modified 2017-04-31 by Ture Teknolog
-
-   For copyright and licensing, see file COPYING */
-
-#include <stdint.h>	 /* Declarations of uint_32 and the like */
-#include <pic32mx.h> /* Declarations of system-specific addresses etc */
-#include "mipslab.h" /* Declatations for these labs */
-
-//By Luis, 1000/45
 #define TMR2PERIOD ((80000000 / 256) / 45) /*45fps => 22,222...ms*/
-float delayValue = 80;
 
-// by Alexander
+float delayValue = 80;
 int referenceX = 15;
 int referenceY = 10;
-
 int obstaclesX[] = {128, 180, 240, 300};
 int obsy[] = {31, 0, 0, 31};
-
 int ceiling = 0;
 int floor = 0;
-// by Luis
 int gameStateBool = 1;
-
 volatile int *portE = (volatile int *)0xbf886110;
 
 void user_isr(void)
 {
-	IFSCLR(0) = 0x100;
-	interruptBool = 1;
+    IFSCLR(0) = 0x100;
+    interruptBool = 1;
 }
 
-/* Lab-specific initialization goes here */
 void labinit(void)
 {
-	volatile int *trisE = (volatile int *)0xbf886100;
-
-	*trisE = *trisE & 0xFF00;
-
-	TRISD = TRISD & 0x0fe0;
-
-	PR2 = TMR2PERIOD;
-	TMR2 = 0;		   
-	T2CONSET = 0X8070; 
-
-	IEC(0) = 0x100; 
-
-	IPC(2) = 0x1f; 
-
-	enable_interrupt();
-	return;
+    volatile int *trisE = (volatile int *)0xbf886100;
+    *trisE = *trisE & 0xFF00;
+    TRISD = TRISD & 0x0fe0;
+    PR2 = TMR2PERIOD;
+    TMR2 = 0;
+    T2CONSET = 0X8070;
+    IEC(0) = 0x100;
+    IPC(2) = 0x1f;
+    enable_interrupt();
+    return;
 }
 
-/* This function is called repetitively from the main program */
+int checkCollision() {
+    int i;
+    for (i = 0; i < 4; i++) {
+        if (((referenceY - 1) >= obsy[i] - 14 && (referenceX + 5) == obstaclesX[i]) ||
+            (((referenceY - 1) <= (obsy[i] + 10)) && ((referenceX + 5) == obstaclesX[i])) ||
+            (((referenceY - 1) <= obsy[i] + 19) && ((referenceX + 5) == obstaclesX[i])) ||
+            (((referenceY - 1) >= (obsy[i] - 18)) && ((referenceX + 5) == obstaclesX[i]))) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void restartGame() {
+    delayValue = 80;
+    *portE = 0;
+    referenceY = 15;
+    referenceX = 10;
+    obstaclesX[0] = 128;
+    obstaclesX[1] = 180;
+    obstaclesX[2] = 240;
+    obstaclesX[3] = 300;
+}
+
 void labwork(void)
 {
-
-	while (gameStateBool)
-	{
-
-		display_string(0, "Flappy square");
-		display_string(2, "BTN4 to gameStateBool");
-		display_string(3, "BTN3 to jump ");
-
-		display_update();
-
-		if (getbtns() == 4)
-		{
-			gameStateBool = 0;
-		}
-	}
-	display_string(0, "");
-
-	// TODO lägg till ghost mode/hard mode ifall man pressar en knapp i gameStateBool screen
-
-	DrawBoxCharacter(referenceX, referenceY);
-
-	// By Alexander
-	MarkObstacles(obstaclesX[0], obsy[0], 14, -1);	// Downwards
-	MarkObstacles(obstaclesX[1], obsy[1], 10, 1);	// Upwards
-	MarkObstacles(obstaclesX[2], obsy[2], 19, 1);	// Upwards
-	MarkObstacles(obstaclesX[3], obsy[3], 18, -1); // Downwards
-
-	DrawCeilingAndFloor(ceiling, floor);
-	
-	display_image(0, icon);
-	
-	MakeScreenBlack();
-
-	referenceY = referenceY + 1;
-
-	// by Luis
-	
-	delay(delayValue);
-	delayValue -= 0.1;
-
-	obstaclesX[0] = obstaclesX[0] - 1;
-	obstaclesX[1] = obstaclesX[1] - 1;
-	obstaclesX[2] = obstaclesX[2] - 1;
-	obstaclesX[3] = obstaclesX[3] - 1;
-
-	if (obstaclesX[0] <= -128)
-	{
-		obstaclesX[0] = 128;
-	}
-
-	if (obstaclesX[1] <= -128)
-	{
-		obstaclesX[1] = 128;
-	}
-
-	if (obstaclesX[2] <= -128)
-	{
-		obstaclesX[2] = 128;
-	}
-
-	if (obstaclesX[3] <= -128)
-	{
-		obstaclesX[3] = 128;
-	}
-	
-
-	if (referenceX == obstaclesX[0])
-	{
-		*portE = *portE + 1;
-	}
-	if (referenceX == obstaclesX[1])
-	{
-		*portE = *portE + 1;
-	}
-	if (referenceX == obstaclesX[2])
-	{
-		*portE = *portE + 1;
-	}
-	if (referenceX == obstaclesX[3])
-	{
-		*portE = *portE + 1;
-	}
-
-	if (getbtns() == 2)
-	{
-		referenceY = referenceY - 3;
-	}
-
-	int isCollisionDetected(int referenceY, int referenceX, int* obsy, int* obstaclesX) {
-		return referenceY >= 30 || referenceY <= 0 || 
-			   ((referenceY - 1) >= obsy[0] - 14 && (referenceX + 5) == obstaclesX[0]) ||
-			   (((referenceY - 1) <= (obsy[1] + 10)) && ((referenceX + 5) == obstaclesX[1])) || 
-			   (((referenceY - 1) <= obsy[2] + 19) && ((referenceX + 5) == obstaclesX[2])) || 
-			   (((referenceY - 1) >= (obsy[3] - 18)) && ((referenceX + 5) == obstaclesX[3]));
-	}
-
-	void resetGame(int* delayValue, int* portE, int* referenceY, int* referenceX, int* obstaclesX) {
-		*delayValue = 80;
-		*portE = 0;
-		*referenceY = 15;
-		*referenceX = 10;
-		obstaclesX[0] = 128;
-		obstaclesX[1] = 180;
-		obstaclesX[2] = 240;
-		obstaclesX[3] = 300;
-	}
-
-	void handleGameOver(int* delayValue, int* portE, int* referenceY, int* referenceX, int* obstaclesX) {
-		int whileloop = 1;
-		while (whileloop)
-		{
-			display_string(0, "Game over");
-			display_string(2, "BTN4 to restart");
-			display_string(1, "");
-			display_update();
-			if (getbtns() == 4)
-			{
-				resetGame(delayValue, portE, referenceY, referenceX, obstaclesX);
-				whileloop = 0;
-			}
-		}
-	}
-
-	// In your main function or wherever you're checking for collisions
-	if (isCollisionDetected(referenceY, referenceX, obsy, obstaclesX)) {
-		handleGameOver(&delayValue, portE, &referenceY, &referenceX, obstaclesX);
-	}
+    while (gameStateBool)
+    {
+        display_string(0, "Flappy square");
+        display_string(2, "BTN4 to gameStateBool");
+        display_string(3, "BTN3 to jump ");
+        display_update();
+        if (getbtns() == 4)
+        {
+            gameStateBool = 0;
+        }
+    }
+    display_string(0, "");
+    DrawBoxCharacter(referenceX, referenceY);
+    MarkObstacles(obstaclesX[0], obsy[0], 14, -1);  // Downwards
+    MarkObstacles(obstaclesX[1], obsy[1], 10, 1);   // Upwards
+    MarkObstacles(obstaclesX[2], obsy[2], 19, 1);   // Upwards
+    MarkObstacles(obstaclesX[3], obsy[3], 18, -1); // Downwards
+    DrawCeilingAndFloor(ceiling, floor);
+    display_image(0, icon);
+    MakeScreenBlack();
+    referenceY = referenceY + 1;
+    delay(delayValue);
+    delayValue -= 0.1;
+    obstaclesX[0] = obstaclesX[0] - 1;
+    obstaclesX[1] = obstaclesX[1] - 1;
+    obstaclesX[2] = obstaclesX[2] - 1;
+    obstaclesX[3] = obstaclesX[3] - 1;
+    if (obstaclesX[0] <= -128) obstaclesX[0] = 128;
+    if (obstaclesX[1] <= -128) obstaclesX[1] = 128;
+    if (obstaclesX[2] <= -128) obstaclesX[2] = 128;
+    if (obstaclesX[3] <= -128) obstaclesX[3] = 128;
+    if (referenceX == obstaclesX[0] || referenceX == obstaclesX[1] ||
+        referenceX == obstaclesX[2] || referenceX == obstaclesX[3]) {
+        *portE += 1;
+    }
+    if (getbtns() == 2) {
+        referenceY = referenceY - 3;
+    }
+    if (referenceY >= 30 || referenceY <= 0 || checkCollision()) {
+        while (1) {
+            display_string(0, "Game over");
+            display_string(2, "BTN4 to restart");
+            display_string(1, "");
+            display_update();
+            if (getbtns() == 4) {
+                restartGame();
+                break;
+            }
+        }
+    }
 }
