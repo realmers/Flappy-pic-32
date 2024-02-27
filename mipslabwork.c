26d127ab6b86bf672c39c9dd37c7eeea26ad57ce
Alexander Danho <danhoalexander123@gmail.com>
2024-02-27 16:37:43 +0100

    comment

--- mipslabwork.c
+++ mipslabwork.c
@@ -1,3 +1,15 @@
+/* mipslabwork.c
+
+   This file written 2015 by F Lundevall
+   Updated 2017-04-21 by F Lundevall
+
+   This file should be changed by YOU! So you must
+   add comment(s) here with your name(s) and date(s):
+
+   This file modified 2017-04-31 by Ture Teknolog
+
+   For copyright and licensing, see file COPYING */
+
 #include <stdint.h>
 #include <pic32mx.h>
 #include "mipslab.h"
